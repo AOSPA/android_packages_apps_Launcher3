019a21c77db8256e860d5e8696a4a0001a3c03dd
Thecrazyskull <anaskarbila@aospa.co>
2016-12-24 06:04:50 -0600

    Launcher3: Support customizable accent colors and dark mode
    Change-Id: I44e8910fe5f06d28b20835949ee9174112a2c730

--- src/com/android/launcher3/allapps/AllAppsGridAdapter.java
+++ src/com/android/launcher3/allapps/AllAppsGridAdapter.java
@@ -343,6 +343,8 @@ public class AllAppsGridAdapter extends RecyclerView.Adapter<AllAppsGridAdapter.
 
     private int mAppsPerRow;
 
+    private static int sSecondaryColor;
+
     private BindViewCallback mBindViewCallback;
     private AllAppsSearchBarController mSearchController;
     private OnFocusChangeListener mIconFocusListener;
@@ -373,6 +375,10 @@ public class AllAppsGridAdapter extends RecyclerView.Adapter<AllAppsGridAdapter.
         mSectionTextPaint.setTextSize(res.getDimensionPixelSize(
                 R.dimen.all_apps_grid_section_text_size));
         mSectionTextPaint.setColor(Utilities.getColorAccent(launcher));
+    }
+
+    public static void setColor(int color) {
+        sSecondaryColor = color;
     }
 
     public static boolean isDividerViewType(int viewType) {
@@ -453,6 +459,7 @@ public class AllAppsGridAdapter extends RecyclerView.Adapter<AllAppsGridAdapter.
             case VIEW_TYPE_PREDICTION_ICON: {
                 BubbleTextView icon = (BubbleTextView) mLayoutInflater.inflate(
                         R.layout.all_apps_icon, parent, false);
+                icon.setTextColor(sSecondaryColor);
                 icon.setOnClickListener(mIconClickListener);
                 icon.setOnLongClickListener(mIconLongClickListener);
                 icon.setLongPressTimeout(ViewConfiguration.get(parent.getContext())
@@ -500,6 +507,7 @@ public class AllAppsGridAdapter extends RecyclerView.Adapter<AllAppsGridAdapter.
             case VIEW_TYPE_ICON: {
                 AppInfo info = mApps.getAdapterItems().get(position).appInfo;
                 BubbleTextView icon = (BubbleTextView) holder.mContent;
+                icon.setTextColor(sSecondaryColor);
                 icon.applyFromApplicationInfo(info);
                 icon.setAccessibilityDelegate(mLauncher.getAccessibilityDelegate());
                 break;
@@ -507,6 +515,7 @@ public class AllAppsGridAdapter extends RecyclerView.Adapter<AllAppsGridAdapter.
             case VIEW_TYPE_PREDICTION_ICON: {
                 AppInfo info = mApps.getAdapterItems().get(position).appInfo;
                 BubbleTextView icon = (BubbleTextView) holder.mContent;
+                icon.setTextColor(sSecondaryColor);
                 icon.applyFromApplicationInfo(info);
                 icon.setAccessibilityDelegate(mLauncher.getAccessibilityDelegate());
                 break;
@@ -514,11 +523,13 @@ public class AllAppsGridAdapter extends RecyclerView.Adapter<AllAppsGridAdapter.
             case VIEW_TYPE_EMPTY_SEARCH:
                 TextView emptyViewText = (TextView) holder.mContent;
                 emptyViewText.setText(mEmptySearchMessage);
+                emptyViewText.setTextColor(sSecondaryColor);
                 emptyViewText.setGravity(mApps.hasNoFilteredResults() ? Gravity.CENTER :
                         Gravity.START | Gravity.CENTER_VERTICAL);
                 break;
             case VIEW_TYPE_SEARCH_MARKET:
                 TextView searchView = (TextView) holder.mContent;
+                searchView.setTextColor(sSecondaryColor);
                 if (mMarketSearchIntent != null) {
                     searchView.setVisibility(View.VISIBLE);
                 } else {
