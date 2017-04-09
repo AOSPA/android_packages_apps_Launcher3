f564c081bd073b314ec70f9dce70f8871be9bda8
Anas Karbila <anaskarbila@gmail.com>
2017-04-09 20:26:04 +0900

    Launcher3: support changing icons and app label
    This introduces a new tab called customization.
    It can be accessed by longpressing the homescreen and pressing the paint icon.
    It supports selecting a custom icon pack.
    Once a custom icon pack has been selected you can drag any icon you want to the edit drop target.
    Once done, it will launch a dialog allowing you to change the app name.
    When pressing on the app icon inside the dialog, an activity will launch giving you the option to select a new icon for your current app.
    
    Change-Id: I4a55dfb2ca42be20d2e81e7f8ec2a8ae207ee165
    Signed-off-by: Anas Karbila <anaskarbila@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -149,6 +149,7 @@ public final class Utilities {
             TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
     public static final String ALLOW_ROTATION_PREFERENCE_KEY = "pref_allowRotation";
+    public static final String KEY_ICON_PACK = "icon-packs";
 
     public static boolean isPropertyEnabled(String propertyName) {
         return Log.isLoggable(propertyName, Log.VERBOSE);
@@ -252,6 +253,16 @@ public final class Utilities {
             return icon;
         }
         return createIconBitmap(new BitmapDrawable(context.getResources(), icon), context);
+    }
+
+    public static Bitmap createBadgedBitmap(Bitmap bitmap,
+            UserHandleCompat user, Context context, int count) {
+        // create the unread icon's info here
+        if (isUnreadCountEnabled(context) && count > 0) {
+            bitmap = createIconBitmapUnreadInfo(context, bitmap, count);
+        }
+
+        return badgeIconForUser(bitmap, user, context);
     }
 
     /**
