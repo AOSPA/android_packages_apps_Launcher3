0d248bef1c2fd9a4547e2f9e850ada3d6c3420c5
enricocid <enrico2588@gmail.com>
2017-09-03 18:31:57 +0200

    Launcher3: add preference to force round icons
    Change-Id: Ia18f92d0014baf28a998111f7125a461673d95c1
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -150,7 +150,8 @@ public final class Utilities {
 
     public static final String ALLOW_ROTATION_PREFERENCE_KEY = "pref_allowRotation";
     public static final String KEY_ICON_PACK = "icon-packs";
-
+    public static final String KEY_ROUND_ICONS = "round-icons";
+	
     public static boolean isPropertyEnabled(String propertyName) {
         return Log.isLoggable(propertyName, Log.VERBOSE);
     }
@@ -159,6 +160,10 @@ public final class Utilities {
         return getPrefs(context).getBoolean(ALLOW_ROTATION_PREFERENCE_KEY,
                 getAllowRotationDefaultValue(context));
     }
+
+	public static boolean isRoundIconsPrefEnabled(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_ROUND_ICONS, false);
+	}
 
     public static boolean getAllowRotationDefaultValue(Context context) {
         if (isNycOrAbove()) {
