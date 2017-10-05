73dceae51d284019361b57e9efdc661da0443184
enricocid <enrico2588@gmail.com>
2017-10-05 19:15:07 +0200

    Launcher3: introduce new edit icon view
    The view is shown using circular reveal when the user drop the icon on
    EditDropTarget.
    The top translucent view is made up of:
    - A centered ImageView/EditText showing the applied icon/label;
    - 3 action buttons:
    close, undo (to apply the default icon for icon pack) and apply button
    to save custom info to database;
    The bottom solid view is made up of:
    - An ImageView/TextView showing default icon and label. This one is clickable
    and will update the icon preview on the top view. User can then apply the
    change using apply button;
    - A RecyclerView showing available icon packs. On icon pack click user will
    be redirected to ChooserIconActivity when the reveal back animation ends;
    
    Change-Id: I9b5ba68a0dab48dde1ffd8072470043aa7d26651
    Signed-off-by: enricocid <enrico2588@gmail.com>

---
+++ src/com/android/launcher3/IconPacksRecyclerViewAdapter.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2017 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+class IconPacksRecyclerViewAdapter extends RecyclerView.Adapter<IconPacksRecyclerViewAdapter.SimpleViewHolder> {
+
+    private Pair<List<String>, List<String>> mIconPacks;
+    private Launcher mLauncher;
+    private boolean mNoIconPacks;
+
+    IconPacksRecyclerViewAdapter(Launcher launcher, Pair<List<String>, List<String>> iconPacks, boolean noIconPacks) {
+
+        mLauncher = launcher;
+        mIconPacks = iconPacks;
+        mNoIconPacks = noIconPacks;
+    }
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        //inflate recycler view item layout
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.icon_item, parent, false);
+        return new SimpleViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleViewHolder holder, int position) {
+
+        ApplicationInfo applicationInfo;
+        PackageManager packageManager = mLauncher.getPackageManager();
+
+        Drawable icon = mLauncher.getResources().getDrawable(R.drawable.ic_google_play_dark, mLauncher.getTheme());
+
+        if (!mNoIconPacks) {
+            try {
+                applicationInfo = packageManager.getApplicationInfo(mIconPacks.first.get(holder.getAdapterPosition()), 0);
+                icon = packageManager.getApplicationIcon(applicationInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        holder.iconPackIcon.setImageDrawable(icon);
+        holder.iconPackLabel.setText(mIconPacks.second.get(holder.getAdapterPosition()));
+    }
+
+    @Override
+    public int getItemCount() {
+
+        //get icon packs list size
+        return mIconPacks.second.size();
+    }
+
+    //simple view holder implementing click listener and with itemView as arguments
+    class SimpleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView iconPackIcon;
+        private TextView iconPackLabel;
+
+        SimpleViewHolder(View itemView) {
+            super(itemView);
+
+            //get the views here
+            iconPackIcon = (ImageView) itemView.findViewById(R.id.app_icon);
+            iconPackLabel = (TextView) itemView.findViewById(R.id.app_label);
+
+            //enable click
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            if (mNoIconPacks) {
+                RevealEditView.close(mLauncher, true, false, null);
+            } else {
+                mLauncher.openIconChooserForIconPack(mIconPacks.first.get(getAdapterPosition()));
+            }
+        }
+    }
+}
