2eeccafeadb647892105214c137bd02dd000d1b0
ezio84 <brabus84@gmail.com>
2018-11-07 23:48:51 +0100

    Launcher3: Add kill action to app shortcuts popup
    Squashed:
    
    Launcher3: Dismiss task from recents when killed
    
    Change-Id: I25f37484a444346bf08e6960893892cc4bf25ada
    
    Launcher3: Add FORCE_STOP_PACKAGES permission
    
    Change-Id: I20dd0287c47269ccfdd02a8eae2a3da1de96e206
    
    Launcher3: Update kill app button drawable
    
    Change-Id: Iff78ae0528b8ed9c7b11952352e4b6d43507e233
    
    Co-authored-by: Ali B <abittin@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
    [mickaelmendes50: Improve 'kill' action string]
    [ghostrider-reborn: Add privapp permissions, adapt to Sv2, 13]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Iff78ae0528b8ed9c7b11952352e4b6d43507e233

--- quickstep/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -22,7 +22,9 @@ import static android.content.Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SYSTEM_SHORTCUT_FREE_FORM_TAP;
 
 import android.app.Activity;
+import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
+import android.app.IActivityManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -30,11 +32,13 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowManagerGlobal;
 import android.window.SplashScreen;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -437,4 +441,49 @@ public interface TaskShortcutFactory {
             return createSingletonShortcutList(modalStateSystemShortcut);
         }
     };
+
+    TaskShortcutFactory KILL_APP = new TaskShortcutFactory() {
+        public List<SystemShortcut> getShortcuts(BaseDraggingActivity activity,
+                                    TaskIdAttributeContainer taskContainer) {
+        String packageName = taskContainer.getTaskView()
+                .getItemInfo().getTargetComponent().getPackageName();
+        return Collections.singletonList(new KillSystemShortcut(activity, taskContainer, packageName));
+
+	}
+    };
+
+    class KillSystemShortcut extends SystemShortcut {
+        private static final String TAG = "KillSystemShortcut";
+        private final TaskView mTaskView;
+        private final BaseDraggingActivity mActivity;
+        private final String mPackageName;
+
+        public KillSystemShortcut(BaseDraggingActivity activity,
+                TaskIdAttributeContainer taskContainer, String packageName) {
+            super(R.drawable.ic_kill_app, R.string.recent_task_option_kill_app,
+                    activity, taskContainer.getItemInfo(), taskContainer.getTaskView());
+            mTaskView = taskContainer.getTaskView();
+            mActivity = activity;
+            mPackageName = packageName;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mPackageName != null) {
+                IActivityManager iam = ActivityManagerNative.getDefault();
+                Task task = mTaskView.getTask();
+                if (task != null) {
+                    try {
+                        iam.forceStopPackage(mPackageName, UserHandle.USER_CURRENT);
+                        Toast appKilled = Toast.makeText(mActivity, R.string.recents_app_killed,
+                            Toast.LENGTH_SHORT);
+                        appKilled.show();
+                        ((RecentsView)mActivity.getOverviewPanel())
+                              .dismissTask(mTaskView, true /* animate */, true /* removeTask */);
+                    } catch (RemoteException e) { }
+                }
+            }
+            dismissTaskMenuView(mActivity);
+        }
+    }
 }
