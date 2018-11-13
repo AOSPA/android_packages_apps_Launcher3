68c4c5eac3a3fe25f31a902ab223c4d195129a47
Shubham Singh <coolsks94@gmail.com>
2018-11-13 12:43:40 +0530

    Launcher3: Allow users to toggle legacy icon masking
    After r16 rebase, commit https://github.com/AospExtended/platform_packages_apps_Launcher3/commit/0d84d7de61128e110ae941185eb9ca41ffa2b22f enforces icon mask to all non adaptive icons,
    causing ugly UX specially with custom icon packs, so simply make it toggleable by user and disable it by default.
    
    Thanks to @eldainosor for pointing me to the commit causing it
    
    Change-Id: Ia4a3e394874391dca1d72aad4d3d162f64ccbe21
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2487,7 +2487,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
                 }
             }
         }
-        if ("pref_iconPackPackage".equals(key)) {
+        if ("pref_iconPackPackage".equals(key) || Icons.KEY_PREF_LEGACY_ICON_MASK.equals(key)) {
             mModel.clearIconCache();
             mModel.forceReload();
         }
