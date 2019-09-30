1aac4e2add2e9466d899709c82ed5bb27733954e
Chris Crump <chris@cypheros.co>
2019-09-29 21:17:44 -0400

    Launcher3: Create Paranoid Launcher
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I33786e5706b5476e5a14c32e96229163f5ffd554

---
+++ quickstep/src/co/aospa/launcher/ParanoidQuickstepLauncher.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020-2024 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.launcher
+
+import com.android.launcher3.uioverrides.QuickstepLauncher
+
+class ParanoidQuickstepLauncher : QuickstepLauncher() {
+
+    companion object {
+        private const val TAG = "ParanoidQuickstepLauncher"
+    }
+
+}
---
+++ src/co/aospa/launcher/ParanoidLauncher.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020-2024 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.launcher
+
+import com.android.launcher3.Launcher
+
+class ParanoidLauncher : Launcher() {
+
+    companion object {
+        private const val TAG = "ParanoidLauncher"
+    }
+
+}
