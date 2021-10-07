55593e4dfbc5701f406e8cabadd5cc8cca173d44
Danny Lin <danny@kdrag0n.dev>
2021-10-06 20:17:10 -0700

    Launcher3: Implement LocalColorExtractor using monet theme engine
    This is a squash of the following commits:
    
    Author:     Anay Wadhera <anay1018@gmail.com>
    AuthorDate: 2022-03-08 09:34:57 +0900
    Launcher3: Implement LocalColorExtractor using monet theme engine
    
    This adds an implementation of the LocalColorExtractor interface,
    including:
      - Local color extraction using WallpaperManager
      - Resource overlay generation
      - View bounds calculation
        This code comes from AOSP commit 88b7f6a0d ("Move color extraction
        utils to extractor class so each instance can have its own set of
        temp variables.") where it was removed. It appears to be virtually
        identical to the Pixel implementation of LocalColorExtractor as of
        SPB5.210812.002.
    
    These features are necessary for local widget colors, as well as local
    colors for popup menu items.
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445961943856992258
    
    [TheScaratic]: Modify to use Aosp monet Theming solution
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I50fb727f4dd06573cb8702f11acf118b4e76e265
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    
    Author:     Anay Wadhera <anay1018@gmail.com>
    AuthorDate: 2022-03-08 09:34:57 +0900
    LocalColorExtractor: Adapt for 12L changes
    
    Required after 69a8eecdc9f80ba7105404be0760fbfbff15258b ("Simplifying color extraction registration logic")
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I9056886b6702d8de6776af542fa55458a675966f
    
    Author:     Danny Lin <danny@kdrag0n.dev>
    AuthorDate: 2022-02-17 23:04:58 -0800
    LocalColorExtractor: Disable local colors when using system presets
    
    Similar to Pixel Launcher, local colors should be disabled when using a
    preset color because presets will most likely differ from the wallpaper.
    This is necessary for WallpaperPickerGoogle and Repainter to work properly.
    
    Reloading works fine as LocalColorExtractor is recreated on
    configuration changes.
    
    Change-Id: I8442b60f8b2359d6d2a0e4007e96c525f3cedc96
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    I50fb727f4dd06573cb8702f11acf118b4e76e265
    
    Change-Id: I61aef5785e71aec02012976167e9149b4cf85815

---
+++ src/com/android/launcher3/widget/LocalWallpaperColorsExtractor.java
@@ -0,0 +1,211 @@
+package com.android.launcher3.widget;
+
+import android.app.WallpaperColors;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.provider.Settings;
+import android.util.SparseIntArray;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.views.ActivityContext;
+import com.android.launcher3.widget.LocalColorExtractor;
+import com.android.systemui.monet.ColorScheme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LocalWallpaperColorsExtractor extends LocalColorExtractor implements
+        WallpaperManager.LocalWallpaperColorConsumer {
+    private static final String KEY_COLOR_SOURCE = "android.theme.customization.color_source";
+
+    private final WallpaperManager wallpaperManager;
+    private Listener listener;
+    private Context mContext;
+
+    private boolean applyOverlay = true;
+
+    // For calculating and returning bounds
+    private final RectF tempRectF = new RectF();
+
+    public static final int[] accent = {
+            android.R.color.system_accent1_10,
+            android.R.color.system_accent1_50,
+            android.R.color.system_accent1_100,
+            android.R.color.system_accent1_200,
+            android.R.color.system_accent1_300,
+            android.R.color.system_accent1_400,
+            android.R.color.system_accent1_500,
+            android.R.color.system_accent1_600,
+            android.R.color.system_accent1_700,
+            android.R.color.system_accent1_800,
+            android.R.color.system_accent1_900,
+            android.R.color.system_accent1_1000,
+            android.R.color.system_accent2_10,
+            android.R.color.system_accent2_50,
+            android.R.color.system_accent2_100,
+            android.R.color.system_accent2_200,
+            android.R.color.system_accent2_300,
+            android.R.color.system_accent2_400,
+            android.R.color.system_accent2_500,
+            android.R.color.system_accent2_600,
+            android.R.color.system_accent2_700,
+            android.R.color.system_accent2_800,
+            android.R.color.system_accent2_900,
+            android.R.color.system_accent2_1000,
+            android.R.color.system_accent3_10,
+            android.R.color.system_accent3_50,
+            android.R.color.system_accent3_100,
+            android.R.color.system_accent3_200,
+            android.R.color.system_accent3_300,
+            android.R.color.system_accent3_400,
+            android.R.color.system_accent3_500,
+            android.R.color.system_accent3_600,
+            android.R.color.system_accent3_700,
+            android.R.color.system_accent3_800,
+            android.R.color.system_accent3_900,
+            android.R.color.system_accent3_1000
+    };
+
+    public static final int[] neutral = {
+            android.R.color.system_neutral1_10,
+            android.R.color.system_neutral1_50,
+            android.R.color.system_neutral1_100,
+            android.R.color.system_neutral1_200,
+            android.R.color.system_neutral1_300,
+            android.R.color.system_neutral1_400,
+            android.R.color.system_neutral1_500,
+            android.R.color.system_neutral1_600,
+            android.R.color.system_neutral1_700,
+            android.R.color.system_neutral1_800,
+            android.R.color.system_neutral1_900,
+            android.R.color.system_neutral1_1000,
+            android.R.color.system_neutral2_10,
+            android.R.color.system_neutral2_50,
+            android.R.color.system_neutral2_100,
+            android.R.color.system_neutral2_200,
+            android.R.color.system_neutral2_300,
+            android.R.color.system_neutral2_400,
+            android.R.color.system_neutral2_500,
+            android.R.color.system_neutral2_600,
+            android.R.color.system_neutral2_700,
+            android.R.color.system_neutral2_800,
+            android.R.color.system_neutral2_900,
+            android.R.color.system_neutral2_1000
+    };
+
+    public LocalWallpaperColorsExtractor(Context context) {
+        mContext = context;
+        wallpaperManager = (WallpaperManager) context.getSystemService(Context.WALLPAPER_SERVICE);
+
+        try {
+            String json = Settings.Secure.getString(context.getContentResolver(),
+                    Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES);
+            if (json != null && !json.isEmpty()) {
+                JSONObject packages = new JSONObject(json);
+                applyOverlay = !"preset".equals(packages.getString(KEY_COLOR_SOURCE));
+            }
+        } catch (JSONException e) {
+            // Ignore: enabled by default
+        }
+    }
+
+    private static void addColorsToArray(List<Integer> list, int[] resArray, SparseIntArray array) {
+        for (int i = 0; i < resArray.length; i++) {
+            array.put(resArray[i], -16777216 | list.get(i));
+        }
+    }
+
+    @Override
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void setWorkspaceLocation(Rect pos, View child, int screenId) {
+        ActivityContext activityContext = (ActivityContext) ActivityContext.lookupContext(child.getContext());
+        if (!(activityContext instanceof Launcher)) {
+            tempRectF.setEmpty();
+            return;
+        }
+        Launcher launcher = (Launcher) activityContext;
+        Resources res = launcher.getResources();
+        DeviceProfile dp = launcher.getDeviceProfile().inv.getDeviceProfile(launcher);
+        float screenWidth = dp.widthPx;
+        float screenHeight = dp.heightPx;
+        int numScreens = launcher.getWorkspace().getNumPagesForWallpaperParallax();
+        float relativeScreenWidth = 1f / numScreens;
+
+        int[] dragLayerBounds = new int[2];
+        launcher.getDragLayer().getLocationOnScreen(dragLayerBounds);
+        // Translate from drag layer coordinates to screen coordinates.
+        int screenLeft = pos.left + dragLayerBounds[0];
+        int screenTop = pos.top + dragLayerBounds[1];
+        int screenRight = pos.right + dragLayerBounds[0];
+        int screenBottom = pos.bottom + dragLayerBounds[1];
+        tempRectF.left = (screenLeft / screenWidth + screenId) * relativeScreenWidth;
+        tempRectF.right = (screenRight / screenWidth + screenId) * relativeScreenWidth;
+        tempRectF.top = screenTop / screenHeight;
+        tempRectF.bottom = screenBottom / screenHeight;
+
+        if (tempRectF.left < 0
+                || tempRectF.right > 1
+                || tempRectF.top < 0
+                || tempRectF.bottom > 1) {
+            tempRectF.setEmpty();
+        }
+
+        if (wallpaperManager != null && !tempRectF.isEmpty()) {
+            wallpaperManager.removeOnColorsChangedListener(this);
+            wallpaperManager.addOnColorsChangedListener(this, new ArrayList<RectF>(List.of(tempRectF)));
+        }
+    }
+
+    @Override
+    public SparseIntArray generateColorsOverride(WallpaperColors colors) {
+        if (!applyOverlay) {
+            return null;
+        }
+
+        SparseIntArray colorRes = new SparseIntArray(5 * 13);
+        boolean nightMode = (mContext.getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+
+        ColorScheme colorScheme = new ColorScheme(ColorScheme.getSeedColor(colors), nightMode);
+
+        addColorsToArray(colorScheme.getAllAccentColors(), accent, colorRes);
+        addColorsToArray(colorScheme.getAllNeutralColors(), neutral, colorRes);
+
+        return colorRes;
+    }
+
+    @Override
+    public void applyColorsOverride(Context base, WallpaperColors colors) {
+        if (!applyOverlay) {
+            return;
+        }
+
+        RemoteViews.ColorResources res =
+                RemoteViews.ColorResources.create(base, generateColorsOverride(colors));
+        if (res != null) {
+            res.apply(base);
+        }
+    }
+
+    @Override
+    public void onColorsChanged(RectF area, WallpaperColors colors) {
+        if (listener != null) {
+            listener.onColorsChanged(generateColorsOverride(colors));
+        }
+    }
+}
