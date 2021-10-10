283408781f2d2661b1f8241effd9948b5c090e57
daniml3 <danimoral1001@gmail.com>
2021-10-11 00:04:29 +0200

    allapps: remove dynamic header background
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I2476bf2ac749c21ad10c46bc9d4ce19d7986171c

--- src/com/android/launcher3/allapps/AllAppsContainerView.java
+++ src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -87,8 +87,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
     public static final float PULL_MULTIPLIER = .02f;
     public static final float FLING_VELOCITY_MULTIPLIER = 1200f;
 
-    private final Paint mHeaderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
     protected final BaseDraggingActivity mLauncher;
     protected final AdapterHolder[] mAH;
     private final ItemInfoMatcher mPersonalMatcher = ItemInfoMatcher.ofUser(Process.myUserHandle());
@@ -131,7 +129,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
     private final int mHeaderProtectionColor;
     private final float mHeaderThreshold;
     private ScrimView mScrimView;
-    private int mHeaderColor;
 
     public AllAppsContainerView(Context context) {
         this(context, null);
@@ -703,18 +700,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
 
     @Override
     public void drawOnScrim(Canvas canvas) {
-        mHeaderPaint.setColor(mHeaderColor);
-        mHeaderPaint.setAlpha((int) (getAlpha() * Color.alpha(mHeaderColor)));
-        if (mHeaderPaint.getColor() != mScrimColor && mHeaderPaint.getColor() != 0) {
-            int bottom = mUsingTabs && mHeader.mHeaderCollapsed ? mHeader.getVisibleBottomBound()
-                    : mSearchContainer.getBottom();
-            canvas.drawRect(0, 0, canvas.getWidth(), bottom + getTranslationY(),
-                    mHeaderPaint);
-
-            if (FeatureFlags.ENABLE_DEVICE_SEARCH.get() && getTranslationY() == 0) {
-                mSearchUiManager.getEditText().setBackground(null);
-            }
-        }
     }
 
     public class AdapterHolder {
@@ -792,18 +777,8 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
 
 
     protected void updateHeaderScroll(int scrolledOffset) {
-        float prog = Math.max(0, Math.min(1, (float) scrolledOffset / mHeaderThreshold));
-        int viewBG = ColorUtils.blendARGB(mScrimColor, mHeaderProtectionColor, prog);
-        int headerColor = ColorUtils.setAlphaComponent(viewBG,
-                (int) (getSearchView().getAlpha() * 255));
-        if (headerColor != mHeaderColor) {
-            mHeaderColor = headerColor;
-            getSearchView().setBackgroundColor(viewBG);
-            getFloatingHeaderView().setHeaderColor(viewBG);
-            invalidateHeader();
-            if (scrolledOffset == 0 && mSearchUiManager.getEditText() != null) {
-                mSearchUiManager.getEditText().show();
-            }
+        if (scrolledOffset == 0 && mSearchUiManager.getEditText() != null) {
+            mSearchUiManager.getEditText().show();
         }
     }
 
