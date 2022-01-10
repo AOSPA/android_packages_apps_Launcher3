246f9f4c9417fc4a0e8640ebf857e9b1f99a94a8
Alex Chau <alexchau@google.com>
2022-01-10 16:13:13 +0000

    Allow items to be added alongside smartspace
    - Allow icons to be added to first row where search container does not occupy
    - On upgrade, clean up first row of screen 0 which may have junk data, as we used to assume first forw of screen 0 is always occupied
    
    Bug: 210127246
    Test: manual
    Change-Id: I835a321964984e02632c8a70474a6551b5e360f7

--- src/com/android/launcher3/model/LoaderCursor.java
+++ src/com/android/launcher3/model/LoaderCursor.java
@@ -536,11 +536,13 @@ public class LoaderCursor extends CursorWrapper {
 
         if (!mOccupied.containsKey(item.screenId)) {
             GridOccupancy screen = new GridOccupancy(countX + 1, countY + 1);
-            if (item.screenId == Workspace.FIRST_SCREEN_ID) {
-                // Mark the first row as occupied (if the feature is enabled)
-                // in order to account for the QSB.
+            if (item.screenId == Workspace.FIRST_SCREEN_ID && FeatureFlags.QSB_ON_FIRST_SCREEN) {
+                // Mark the first X columns (X is width of the search container) in the first row as
+                // occupied (if the feature is enabled) in order to account for the search
+                // container.
+                int spanX = mIDP.numSearchContainerColumns;
                 int spanY = 1;
-                screen.markCells(0, 0, countX + 1, spanY, Utilities.showSmartspace(mContext));
+                screen.markCells(0, 0, spanX, spanY, true);
             }
             mOccupied.put(item.screenId, screen);
         }
