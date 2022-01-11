1cd5f7a6713cf63c2a9dc8c89da67c17e30b249d
Pranav Vashi <neobuddy89@gmail.com>
2022-01-11 11:33:42 +0530

    Launcher3: Fix icon palette crashes
    Change-Id: Iceaa093fba4a71faff8e629cbdc463b3f92f0f66
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/com/android/launcher3/graphics/IconPalette.java
+++ src/com/android/launcher3/graphics/IconPalette.java
@@ -98,8 +98,8 @@ public class IconPalette {
     /** For debugging. This was copied from com.android.internal.util.NotificationColorUtil. */
     private static String contrastChange(int colorOld, int colorNew, int bg) {
         return String.format("from %.2f:1 to %.2f:1",
-                ColorUtils.calculateContrast(colorOld, bg),
-                ColorUtils.calculateContrast(colorNew, bg));
+                calculateContrast(colorOld, bg),
+                calculateContrast(colorNew, bg));
     }
 
     /**
@@ -131,7 +131,7 @@ public class IconPalette {
      * This was copied from com.android.internal.util.NotificationColorUtil.
      */
     private static int findContrastColor(int fg, int bg, double minRatio) {
-        if (ColorUtils.calculateContrast(fg, bg) >= minRatio) {
+        if (calculateContrast(fg, bg) >= minRatio) {
             return fg;
         }
 
@@ -147,12 +147,21 @@ public class IconPalette {
         for (int i = 0; i < 15 && high - low > 0.00001; i++) {
             final double l = (low + high) / 2;
             fg = ColorUtils.LABToColor(l, a, b);
-            if (ColorUtils.calculateContrast(fg, bg) > minRatio) {
+            if (calculateContrast(fg, bg) > minRatio) {
                 if (isBgDark) high = l; else low = l;
             } else {
                 if (isBgDark) low = l; else high = l;
             }
         }
         return ColorUtils.LABToColor(low, a, b);
+    }
+
+    private static double calculateContrast(int fg, int bg) {
+        bg = setAlphaComponent(bg, 255);
+        return ColorUtils.calculateContrast(fg, bg);
+    }
+
+    private static int setAlphaComponent(int color, int alpha) {
+        return (color & 0x00ffffff) | (alpha << 24);
     }
 }
