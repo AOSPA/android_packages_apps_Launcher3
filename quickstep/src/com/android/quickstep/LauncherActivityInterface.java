fe48bdae7fe29356346301e721b6cec6138a0fed
Pranav Santhosh <59979143+npv12@users.noreply.github.com>
2022-01-29 15:15:34 +0530

    Launcher3: Return if launcher is null in notifyRecentsOfOrientation
    Change-Id: Ia69fb0e6d9bfa51cd8be456373f0cc3510a50e04

--- quickstep/src/com/android/quickstep/LauncherActivityInterface.java
+++ quickstep/src/com/android/quickstep/LauncherActivityInterface.java
@@ -260,7 +260,11 @@ public final class LauncherActivityInterface extends
 
     private void notifyRecentsOfOrientation(RotationTouchHelper rotationTouchHelper) {
         // reset layout on swipe to home
-        RecentsView recentsView = getCreatedContainer().getOverviewPanel();
+        Launcher launcher = getCreatedContainer();
+        if (launcher == null) {
+            return;
+        }
+        RecentsView recentsView = launcher.getOverviewPanel();
         recentsView.setLayoutRotation(rotationTouchHelper.getCurrentActiveRotation(),
                 rotationTouchHelper.getDisplayRotation());
     }
