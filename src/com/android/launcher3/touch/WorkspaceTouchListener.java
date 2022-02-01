33695eae4e67ac600659d599bc7f427d8e2c91f3
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-02-01 19:06:26 +0530

    fixup! Launcher3: Add toggle for DT2S
    Change-Id: I2e537fa8641503d4ddfb10cad79e7d5fd0db3f2b
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -69,27 +69,30 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
     private final Workspace mWorkspace;
     private final PointF mTouchDownPoint = new PointF();
     private final float mTouchSlop;
-    private boolean mDoubleTapEnabled;
+
     private int mLongPressState = STATE_CANCELLED;
 
     private final PowerManager mPm;
 
     private final GestureDetector mGestureDetector;
 
+    private final Context mContext;
+
     public WorkspaceTouchListener(Launcher launcher, Workspace workspace) {
         mLauncher = launcher;
         mWorkspace = workspace;
+        mContext = workspace.getContext();
         // Use twice the touch slop as we are looking for long press which is more
         // likely to cause movement.
         mTouchSlop = 2 * ViewConfiguration.get(launcher).getScaledTouchSlop();
-        mPm = (PowerManager) workspace.getContext().getSystemService(Context.POWER_SERVICE);
-        mGestureDetector = new GestureDetector(workspace.getContext(), this);
-        mDoubleTapEnabled = Utilities.isDoubleTapGestureEnabled(workspace.getContext());
+        mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mGestureDetector = new GestureDetector(mContext, this);
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent ev) {
         mGestureDetector.onTouchEvent(ev);
+
         int action = ev.getActionMasked();
         if (action == ACTION_DOWN) {
             // Check if we can handle long press.
@@ -191,8 +194,8 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-      if (mDoubleTapEnabled)
-        mPm.goToSleep(event.getEventTime());
+        if (Utilities.isDoubleTapGestureEnabled(mContext))
+            mPm.goToSleep(event.getEventTime());
         return true;
     }
 }
