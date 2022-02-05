a0b375ffb630cabb22ec56b4d7295ff991be435c
LibXZR <i@xzr.moe>
2022-02-05 16:33:05 +0800

    Launcher3: Introduce overview scrolling scale animation
    This vivid animation enables users to perceive more clearly
    about the switching of the task cards.
    
    Change-Id: Ie1350d6bda2d311f70f9be7cff47b0abe7e00cad
    
    Fix discontinuous animation when dismissing taskcards on
     the right side
    
    Cards should never be scale down in this animation.
    
    Change-Id: I759492390eb8f1a2b9023c2d264f0e7191b17108
    
    Recalculate scroll scale on layout
    
    Edge case exist that when there's only one task,
    onScrollChanged() won't be called so the last scale
    from the recycled view is showed, leading to wrong
    display size.
    
    Change-Id: If8a189ba6b854ed486283d43b8a0ac3e4bb4e9f2
    
    Change-Id: I900366e35782e13e248590e52e8bc95919c47f73

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -20,6 +20,7 @@ import static android.app.ActivityTaskManager.INVALID_TASK_ID;
 import static android.os.Trace.traceBegin;
 import static android.os.Trace.traceEnd;
 import static android.view.Surface.ROTATION_0;
+import static android.view.Surface.ROTATION_180;
 import static android.view.View.MeasureSpec.EXACTLY;
 import static android.view.View.MeasureSpec.makeMeasureSpec;
 
@@ -43,6 +44,7 @@ import static com.android.launcher3.Flags.enableLargeDesktopWindowingTile;
 import static com.android.launcher3.Flags.enableOverviewBackgroundWallpaperBlur;
 import static com.android.launcher3.Flags.enableRefactorTaskThumbnail;
 import static com.android.launcher3.Flags.enableSeparateExternalDisplayTasks;
+import static com.android.launcher3.LauncherAnimUtils.SCALE_PROPERTY;
 import static com.android.launcher3.LauncherAnimUtils.SUCCESS_TRANSITION_PROGRESS;
 import static com.android.launcher3.LauncherAnimUtils.VIEW_ALPHA;
 import static com.android.launcher3.LauncherAnimUtils.VIEW_BACKGROUND_COLOR;
@@ -628,6 +630,8 @@ public abstract class RecentsView<
     private Map<TaskView, Integer> mTaskViewsDismissPrimaryTranslations =
             new HashMap<TaskView, Integer>();
 
+    private float mScrollScale = 1f;
+
     /**
      * TODO: Call reloadIdNeeded in onTaskStackChanged.
      */
@@ -1006,6 +1010,8 @@ public abstract class RecentsView<
         }
         // make sure filter is turned off by default
         mFilterState.setFilterBy(null);
+
+        mScrollScale = getResources().getFloat(R.dimen.overview_scroll_scale);
     }
 
     /** Get the state of the filter */
@@ -4061,6 +4067,14 @@ public abstract class RecentsView<
                     && nextFocusedTaskView == null && !dismissingForSplitSelection)) {
                 int offset = getOffsetToDismissedTask(scrollDiffPerPage, dismissedIndex,
                         lastTaskViewIndex);
+                if (i == dismissedIndex + 1 ||
+                        dismissedIndex == taskCount -1 && i == dismissedIndex - 1) {
+                    if (child.getScaleX() <= dismissedTaskView.getScaleX())
+                        anim.setFloat(child, SCALE_PROPERTY,
+                            dismissedTaskView.getScaleX(), LINEAR);
+                    else
+                        anim.setFloat(child, SCALE_PROPERTY, 1f, LINEAR);
+                }
                 int scrollDiff = newScroll[i] - oldScroll[i] + offset;
                 if (scrollDiff != 0) {
                     if (!isExpressiveDismiss) {
@@ -4994,6 +5008,7 @@ public abstract class RecentsView<
                         .setScroll(getScrollOffset()));
         setImportantForAccessibility(isModal() ? IMPORTANT_FOR_ACCESSIBILITY_NO
                 : IMPORTANT_FOR_ACCESSIBILITY_AUTO);
+        doScrollScale();
     }
 
     private void updatePivots() {
@@ -6901,6 +6916,32 @@ public abstract class RecentsView<
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
         dispatchScrollChanged();
+        doScrollScale();
+    }
+
+    private void doScrollScale() {
+        if (showAsGrid())
+            return;
+
+        boolean isInLandscape = mOrientationState.getTouchRotation() != ROTATION_0
+                                && mOrientationState.getTouchRotation() != ROTATION_180;
+        int childCount = Math.min(mPageScrolls.length, getChildCount());
+        int curScroll = isInLandscape ? getScrollY() : getScrollX();
+
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            int scaleArea = child.getWidth() + mPageSpacing;
+            int childPosition = mPageScrolls[i];
+            int scrollDelta = Math.abs(curScroll - childPosition);
+            if (scrollDelta > scaleArea) {
+                child.setScaleX(mScrollScale);
+                child.setScaleY(mScrollScale);
+            } else {
+                float scale = mapToRange(scrollDelta, 0, scaleArea, 1f, mScrollScale, LINEAR);
+                child.setScaleX(scale);
+                child.setScaleY(scale);
+            }
+        }
     }
 
     /**
