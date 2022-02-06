1f7710cb35247691b22165691264ba5ebc674123
Pranav Vashi <neobuddy89@gmail.com>
2022-02-07 01:50:09 +0530

    Launcher3: Enable lens icon for dock searchbar
    Co-authored-by: Suphon Thanakornpakapong <paphonb@gmail.com>
    Change-Id: I451af20d9164891a5b4450974692cdbe8c1a2f07
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -190,8 +190,6 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
         private boolean mPreferenceHighlighted = false;
         private Preference mDeveloperOptionPref;
 
-        protected static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
-
         private Preference mShowGoogleAppPref;
 
         @Override
@@ -311,17 +309,9 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             return showPreference;
         }
 
-        public static boolean isGSAEnabled(Context context) {
-            try {
-                return context.getPackageManager().getApplicationInfo(GSA_PACKAGE, 0).enabled;
-            } catch (PackageManager.NameNotFoundException e) {
-                return false;
-            }
-        }
-
         private void updateIsGoogleAppEnabled() {
             if (mShowGoogleAppPref != null) {
-                mShowGoogleAppPref.setEnabled(isGSAEnabled(getContext()));
+                mShowGoogleAppPref.setEnabled(Utilities.isGSAEnabled(getContext()));
             }
         }
 
