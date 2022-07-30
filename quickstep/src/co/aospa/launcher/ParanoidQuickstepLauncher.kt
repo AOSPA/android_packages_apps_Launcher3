5a692e0cfc6288a5c216c0ac7340bf04bf411f33
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 02:12:26 +0530

    Launcher3: Implement Smartspace
    Reverse engineered from Pixel Launcher (TP1A).
    
    [HELLBOY017]: Adapt for 15
    
    Co-authored-by: Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Change-Id: Ie8d5cca25e6803bd080f92a47610bccdbde09c5a

--- quickstep/src/co/aospa/launcher/ParanoidQuickstepLauncher.kt
+++ quickstep/src/co/aospa/launcher/ParanoidQuickstepLauncher.kt
@@ -6,10 +6,21 @@
 
 package co.aospa.launcher
 
+import android.app.smartspace.SmartspaceTarget
+import android.os.Bundle
+import co.aospa.launcher.ParanoidLauncherModelDelegate.SmartspaceItem
+import com.android.launcher3.model.BgDataModel
+import com.android.launcher3.qsb.LauncherUnlockAnimationController
 import com.android.launcher3.uioverrides.QuickstepLauncher
 import com.android.systemui.plugins.shared.LauncherOverlayManager
+import com.android.quickstep.SystemUiProxy
+
+import com.google.android.systemui.smartspace.BcSmartspaceDataProvider
 
 class ParanoidQuickstepLauncher : QuickstepLauncher() {
+
+    private val mSmartspacePlugin = BcSmartspaceDataProvider()
+    private val mUnlockAnimationController = LauncherUnlockAnimationController(this)
 
     companion object {
         private const val TAG = "ParanoidQuickstepLauncher"
@@ -19,4 +30,39 @@ class ParanoidQuickstepLauncher : QuickstepLauncher() {
         return OverlayCallbackImpl(this)
     }
 
+    fun getSmartspacePlugin(): BcSmartspaceDataProvider {
+        return mSmartspacePlugin
+    }
+
+    fun getLauncherUnlockAnimationController(): LauncherUnlockAnimationController {
+        return mUnlockAnimationController
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SystemUiProxy.INSTANCE.get(this).setLauncherUnlockAnimationController(this.javaClass.simpleName, mUnlockAnimationController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        SystemUiProxy.INSTANCE.get(this).setLauncherUnlockAnimationController("null", null)
+    }
+
+    override fun onOverlayVisibilityChanged(visible: Boolean) {
+        super.onOverlayVisibilityChanged(visible)
+        mUnlockAnimationController.updateSmartspaceState()
+    }
+
+    override fun onPageEndTransition() {
+        super.onPageEndTransition()
+        mUnlockAnimationController.updateSmartspaceState()
+    }
+
+    override fun bindExtraContainerItems(container: BgDataModel.FixedContainerItems) {
+        if (container.containerId == -110) {
+            val targets = container.items.map { item -> (item as SmartspaceItem).smartspaceTarget }
+            mSmartspacePlugin.onTargetsAvailable(targets)
+        }
+        super.bindExtraContainerItems(container)
+    }
 }
