a66e56ab0877bfc9f1bf1644e98e74cee56e593b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 02:12:26 +0530

    Launcher3: Implement Smartspace
    Reverse engineered from Pixel Launcher (TP1A).
    Fixed up margins.
    
    Change-Id: Ie8d5cca25e6803bd080f92a47610bccdbde09c5a
    
    Author:     Adithya R <gh0strider.2k18.reborn@gmail.com>
    AuthorDate: 2022-11-11 18:20:50 +0530
    Launcher3: Fix crash in smartspace targets dump
    
    D AndroidRuntime: Shutting down VM
    E AndroidRuntime: FATAL EXCEPTION: main
    E AndroidRuntime: Process: com.android.launcher3, PID: 28277
    E AndroidRuntime: java.lang.ClassCastException: java.util.ArrayList cannot be cast to android.app.smartspace.SmartspaceTarget
    E AndroidRuntime: 	at co.aospa.launcher.ParanoidLauncherModelDelegate.lambda$dump$0$co-aospa-launcher-ParanoidLauncherModelDelegate(ParanoidLauncherModelDelegate.java:70)
    E AndroidRuntime: 	at co.aospa.launcher.ParanoidLauncherModelDelegate$$ExternalSyntheticLambda0.accept(Unknown Source:6)
    E AndroidRuntime: 	at java.util.Iterator.forEachRemaining(Iterator.java:133)
    E AndroidRuntime: 	at co.aospa.launcher.ParanoidLauncherModelDelegate.dump(ParanoidLauncherModelDelegate.java:66)
    E AndroidRuntime: 	at com.android.launcher3.LauncherModel.dumpState(LauncherModel.java:651)
    E AndroidRuntime: 	at com.android.launcher3.LauncherProvider.dump(LauncherProvider.java:125)
    E AndroidRuntime: 	at android.app.ActivityThread.handleDumpProvider(ActivityThread.java:4665)
    ...
    
    Change-Id: I5fb62c65f20dde1b0732ce9dfbf2cee91e28ae9b
    
    Author: John Galt <johngaltfirstrun@gmail.com>
    AuthorDate: Thu, 16 Mar 2023 12:34:21 -0400
    fixup Smartspace (4d3b9fa010e5a)
    
    Change-Id: Ia574e32b3b72a35d6be61715fba76fca73026db7
    
    Change-Id: Ie8d5cca25e6803bd080f92a47610bccdbde09c5a

---
+++ src/com/android/launcher3/qsb/SmartspaceViewContainer.java
@@ -0,0 +1,88 @@
+package com.android.launcher3.qsb;
+
+import android.app.smartspace.SmartspaceAction;
+import android.app.smartspace.SmartspaceTarget;
+import android.app.smartspace.SmartspaceTargetEvent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import co.aospa.launcher.ParanoidLauncher;
+import co.aospa.launcher.ParanoidLauncherModelDelegate;
+
+import com.android.launcher3.celllayout.CellLayoutLayoutParams;;
+import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.icons.GraphicsUtils;
+import com.android.launcher3.uioverrides.plugins.PluginManagerWrapper;
+import com.android.launcher3.views.ActivityContext;
+import com.android.quickstep.SystemUiProxy;
+
+import com.android.systemui.plugins.BcSmartspaceDataPlugin;
+import com.android.systemui.plugins.PluginListener;
+
+import com.google.android.systemui.smartspace.BcSmartspaceDataProvider;
+import com.google.android.systemui.smartspace.BcSmartspaceView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartspaceViewContainer extends FrameLayout implements PluginListener<BcSmartspaceDataPlugin> {
+
+    public BcSmartspaceView mView;
+
+    public SmartspaceViewContainer(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        mView = (BcSmartspaceView) inflate(context, R.layout.smartspace_enhanced, null);
+        mView.setPrimaryTextColor(GraphicsUtils.getAttrColor(context, R.attr.workspaceTextColor));
+        LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER_VERTICAL;
+        layoutParams.setMarginStart(getResources().getDimensionPixelSize(R.dimen.enhanced_smartspace_margin_start_launcher));
+        addView(mView, layoutParams);
+
+        ParanoidLauncher launcher = (ParanoidLauncher) ActivityContext.lookupContext(context);
+        launcher.getLauncherUnlockAnimationController().setSmartspaceView(mView);
+
+        ParanoidLauncherModelDelegate delegate = (ParanoidLauncherModelDelegate) launcher.getModel().getModelDelegate();
+        BcSmartspaceDataProvider plugin = launcher.getSmartspacePlugin();
+        plugin.registerSmartspaceEventNotifier(event -> delegate.notifySmartspaceEvent(event));
+        mView.registerDataProvider(plugin);
+    }
+
+    @Override
+    public void onPluginConnected(BcSmartspaceDataPlugin plugin, Context context) {
+        mView.registerDataProvider(plugin);
+    }
+
+    @Override
+    public void onPluginDisconnected(BcSmartspaceDataPlugin plugin) {
+        ParanoidLauncher launcher = (ParanoidLauncher) ActivityContext.lookupContext(getContext());
+        mView.registerDataProvider(launcher.getSmartspacePlugin());
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        PluginManagerWrapper.INSTANCE.get(getContext()).addPluginListener(this, BcSmartspaceDataPlugin.class);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        PluginManagerWrapper.INSTANCE.get(getContext()).removePluginListener(this);
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom) {
+        CellLayoutLayoutParams lp = (CellLayoutLayoutParams) getLayoutParams();
+        lp.setMargins(left, top, right, bottom);
+        setLayoutParams(lp);
+    }
+}
