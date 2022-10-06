7ac8f941da4475cb138f625daa372e6abcad404a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-06 17:51:11 +0530

    Launcher3: Improve search bar header protection
    Before: https://i.imgur.com/emVsSFR.jpg
    After: https://imgur.com/a/m6LJw2y
    
    Change-Id: Ia590bcdab70fbb07b162f0e9e3df2b320a171e0a

--- src/com/android/launcher3/allapps/FloatingHeaderView.java
+++ src/com/android/launcher3/allapps/FloatingHeaderView.java
@@ -326,7 +326,7 @@ public class FloatingHeaderView extends LinearLayout implements
 
         mTabLayout.setTranslationY(mTranslationY);
 
-        int clipTop = getPaddingTop() * 2 - mTabsAdditionalPaddingTop;
+        int clipTop = getPaddingTop() - mTabsAdditionalPaddingTop;
         if (mTabsHidden) {
             // Add back spacing that is otherwise covered by the tabs.
             clipTop += mTabsAdditionalPaddingTop;
