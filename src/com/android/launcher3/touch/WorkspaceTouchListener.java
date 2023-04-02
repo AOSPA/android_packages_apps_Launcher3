d4b492386069a51274d66bbfcd22298f22ea5770
LordShenron <shen.priyanshu@gmail.com>
2023-04-02 20:16:50 +0800

    Launcher3: Vibrate on double tap to sleep
    Change-Id: I613edd50bd51c59fee4a11ee2534ff054ebb6125

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -50,6 +50,7 @@ import com.android.launcher3.logger.LauncherAtom;
 import com.android.launcher3.testing.TestLogging;
 import com.android.launcher3.testing.shared.TestProtocol;
 import com.android.launcher3.util.TouchUtil;
+import com.android.launcher3.util.VibratorWrapper;
 
 import com.android.launcher3.Utilities;
 
@@ -233,8 +234,10 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-        if (Utilities.isDoubleTapGestureEnabled(mContext))
+        if (Utilities.isDoubleTapGestureEnabled(mContext)) {
+            VibratorWrapper.INSTANCE.get(mContext).vibrate(VibratorWrapper.EFFECT_CLICK);
             mPm.goToSleep(event.getEventTime());
+        }
         return true;
     }
 }
