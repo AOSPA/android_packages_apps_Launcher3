2acf7d7e1b639025afdcee88cc18df9fe12a9e8e
TimoDo <timo.capa@gmail.com>
2023-04-15 06:19:14 +0200

    Launcher3: Enable SHOW_DOT_PAGINATION and workaround a crash
    Change-Id: Icaac6b6f0ce57fb7620fb7e79b00bbdf5704850a

--- src/com/android/launcher3/pageindicators/PageIndicatorDots.java
+++ src/com/android/launcher3/pageindicators/PageIndicatorDots.java
@@ -173,6 +173,9 @@ public class PageIndicatorDots extends View implements Insettable, PageIndicator
         mTotalScroll = totalScroll;
 
         int scrollPerPage = totalScroll / (mNumPages - 1);
+        if (scrollPerPage == 0) {
+            scrollPerPage = 1;
+        }
         int pageToLeft = currentScroll / scrollPerPage;
         int pageToLeftScroll = pageToLeft * scrollPerPage;
         int pageToRightScroll = pageToLeftScroll + scrollPerPage;
