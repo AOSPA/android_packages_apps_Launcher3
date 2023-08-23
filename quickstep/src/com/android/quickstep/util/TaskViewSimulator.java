bd33c57e36a83adbb07339180f554a5a96ba859a
nift4 <nift4@protonmail.com>
2023-08-23 18:29:52 +0200

    Launcher3: Disable live tile - fix blur flicker
    As per minaripenguin's now reverted commits
    
    Change-Id: I4fc0918e5b39d225e09841fb2dc53ff52df87932

--- quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
+++ quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
@@ -535,16 +535,7 @@ public class TaskViewSimulator implements TransformParams.BuilderProxy {
             // conflict with layers that WM core positions (ie. the input consumers).  For shell
             // transitions, the animation leashes are reparented to an animation container so we
             // can bump layers as needed.
-            if (ENABLE_SHELL_TRANSITIONS) {
-                builder.setLayer(mDrawsBelowRecents
-                        ? Integer.MIN_VALUE + app.prefixOrderIndex
-                        // 1000 is an arbitrary number to give room for multiple layers.
-                        : Integer.MAX_VALUE - 1000 + app.prefixOrderIndex);
-            } else {
-                builder.setLayer(mDrawsBelowRecents
-                        ? Integer.MIN_VALUE + app.prefixOrderIndex
-                        : 0);
-            }
+            builder.setLayer(0);
         }
     }
 
