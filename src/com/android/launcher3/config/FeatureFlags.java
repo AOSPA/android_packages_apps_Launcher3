7f1b8a73b660955bacba09819c499233239fcc08
TimoDo <timo.capa@gmail.com>
2024-01-10 10:56:08 +0100

    Revert "Launcher3: Enable FORCED_MONO_ICON by default"
    reason: same as to why this wasn't merged in topaz - some app icons become unthemed in the open / close animation and looks bad
    
    see comment in original commit: https://gerrit.aospa.co/c/AOSPA/android_packages_apps_Launcher3/+/29250
    
    https://i.imgur.com/LUmKvD9.mp4
    
    This reverts commit 3e324900a3045f1f517d8eb88d1ede694ecd27f9.
    
    Change-Id: I92409abc951c58c1bab69d02081e7b23d0260801

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -150,7 +150,7 @@ public final class FeatureFlags {
 
     // TODO(Block 7): Clean up flags
     public static final BooleanFlag ENABLE_FORCED_MONO_ICON = getDebugFlag(270396209,
-            "ENABLE_FORCED_MONO_ICON", ENABLED,
+            "ENABLE_FORCED_MONO_ICON", DISABLED,
             "Enable the ability to generate monochromatic icons, if it is not provided by the app");
 
     // TODO(Block 8): Clean up flags
