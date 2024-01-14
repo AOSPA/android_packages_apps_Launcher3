2b7264dfde1ff211cfbac6c94ad60972428e071a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-14 06:27:22 +0530

    Launcher3: Allow hiding top shadow on statusbar
    On certain devices (especially amoled) with certain
    black-themed wallpapers, it looks quite bad. Add a
    toggle to disable it, inspired from lawnchair:
    
    https://github.com/LawnchairLauncher/lawnchair/commit/c99e61205cc4d5b5d3d9b827787c9fd873616060
    https://github.com/LawnchairLauncher/lawnchair/commit/e41e27a4d11e7f8352a3d2fa74afb04e8af9354a
    
    Co-authored-by: Suphon Thanakornpakapong <paphonb@gmail.com>
    Change-Id: I26e2fd8097d8b4855cbd2e433f7893fe240af6dd
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/graphics/SysUiScrim.java
+++ src/com/android/launcher3/graphics/SysUiScrim.java
@@ -21,6 +21,7 @@ import static android.graphics.Paint.FILTER_BITMAP_FLAG;
 import static com.android.launcher3.config.FeatureFlags.KEYGUARD_ANIMATION;
 
 import android.animation.ObjectAnimator;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.LinearGradient;
@@ -36,6 +37,7 @@ import androidx.annotation.VisibleForTesting;
 
 import com.android.launcher3.BaseDraggingActivity;
 import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.anim.AnimatedFloat;
 import com.android.launcher3.testing.shared.ResourceUtils;
@@ -46,7 +48,8 @@ import com.android.launcher3.util.Themes;
 /**
  * View scrim which draws behind hotseat and workspace
  */
-public class SysUiScrim implements View.OnAttachStateChangeListener {
+public class SysUiScrim implements View.OnAttachStateChangeListener,
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     /**
      * Receiver used to get a signal that the user unlocked their device.
@@ -73,6 +76,8 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
     private static final int BOTTOM_MASK_HEIGHT_DP = 200;
     private static final int TOP_MASK_HEIGHT_DP = 100;
 
+    private static final String KEY_HIDE_TOP_SHADOW = "pref_hide_top_shadow";
+
     private boolean mDrawTopScrim, mDrawBottomScrim;
 
     private final RectF mTopMaskRect = new RectF();
@@ -88,6 +93,7 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
     private final View mRoot;
     private final BaseDraggingActivity mActivity;
     private final boolean mHideSysUiScrim;
+    private boolean mHideSysUiScrimSetting;
     private boolean mSkipScrimAnimationForTest = false;
 
     private boolean mAnimateScrimOnNextDraw = false;
@@ -114,13 +120,17 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
         if (!KEYGUARD_ANIMATION.get() && !mHideSysUiScrim) {
             view.addOnAttachStateChangeListener(this);
         }
+
+        SharedPreferences prefs = LauncherPrefs.getPrefs(view.getContext());
+        mHideSysUiScrimSetting = prefs.getBoolean(KEY_HIDE_TOP_SHADOW, false);
+        prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     /**
      * Draw the top and bottom scrims
      */
     public void draw(Canvas canvas) {
-        if (!mHideSysUiScrim) {
+        if (!mHideSysUiScrim && !mHideSysUiScrimSetting) {
             if (mSysUiProgress.value <= 0) {
                 mAnimateScrimOnNextDraw = false;
                 return;
@@ -183,6 +193,14 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
         ScreenOnTracker.INSTANCE.get(mActivity).removeListener(mScreenOnListener);
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(KEY_HIDE_TOP_SHADOW)) {
+            mHideSysUiScrimSetting = prefs.getBoolean(KEY_HIDE_TOP_SHADOW, false);
+            mRoot.invalidate();
+        }
+    }
+
     /**
      * Set the width and height of the view being scrimmed
      */
@@ -202,7 +220,7 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
 
     private void reapplySysUiAlpha() {
         reapplySysUiAlphaNoInvalidate();
-        if (!mHideSysUiScrim) {
+        if (!mHideSysUiScrim && !mHideSysUiScrimSetting) {
             mRoot.invalidate();
         }
     }
