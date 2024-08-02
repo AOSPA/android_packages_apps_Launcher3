2e8cfa2a81b305f0ef6d9d106c6eaf3d66b91a48
Abhay Singh Gill <abhaygill017@gmail.com>
2024-08-03 01:42:27 +0800

    Launcher3: Add Circle To Search
    Requires framework overlays:
    
    <string name="config_defaultContextualSearchPackageName" translatable="false">com.google.android.googlequicksearchbox</string>
    
    Change-Id: Ia0b03781ad579d88e62ef9cc5721fc5cdd0c943b

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -33,6 +33,7 @@ import static co.aospa.launcher.OverlayCallbackImpl.KEY_ICON_SIZE;
 import static co.aospa.launcher.OverlayCallbackImpl.KEY_MINUS_ONE;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -56,6 +57,7 @@ import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCal
 import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartScreenCallback;
 import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.launcher3.BuildConfig;
@@ -68,6 +70,10 @@ import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
+
+import com.android.quickstep.SystemUiProxy;
+import com.android.quickstep.util.AssistUtils;
+
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 /**
@@ -91,6 +97,11 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    private static final String CTS_KEY = "pref_allow_cts";
+    private static boolean mContextualSearchDefValue;
+    private static boolean mCtsEnabled;
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,6 +134,9 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             // Display the fragment as the main content.
             fm.beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, f).commit();
         }
+            mContext = getApplicationContext();
+            mContextualSearchDefValue = mContext.getResources().getBoolean(
+                     com.android.internal.R.bool.config_searchAllEntrypointsEnabledDefault);
             LauncherPrefs.getPrefs(getApplicationContext())
                     .registerOnSharedPreferenceChangeListener(this);
     }
@@ -144,6 +158,13 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             case KEY_FONT_SIZE:
             case KEY_ICON_SIZE:
                 InvariantDeviceProfile.INSTANCE.get(this).onConfigChanged(getApplicationContext());
+                break;
+            case CTS_KEY:
+                mCtsEnabled = LauncherPrefs.getPrefs(mContext).getBoolean(CTS_KEY, mContextualSearchDefValue);
+                Settings.Secure.putInt(mContext.getContentResolver(),
+                        Settings.Secure.SEARCH_ALL_ENTRYPOINTS_ENABLED, mCtsEnabled ? 1 : 0);
+                SystemUiProxy.INSTANCE.get(mContext).setAssistantOverridesRequested(
+                        AssistUtils.newInstance(mContext).getSysUiAssistOverrideInvocationTypes());
                 break;
             default:
                 break;
@@ -205,6 +226,7 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
         private Preference mShowGoogleAppPref;
         private Preference mShowGoogleBarPref;
+        private SwitchPreference mCtsPref;
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -239,6 +261,15 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
+            }
+
+            mCtsEnabled = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.SEARCH_ALL_ENTRYPOINTS_ENABLED, mContextualSearchDefValue ? 1 : 0) == 1;
+            mCtsPref = (SwitchPreference) findPreference(CTS_KEY);
+            if (!AssistUtils.newInstance(mContext).isContextualSearchIntentAvailable()) {
+                getPreferenceScreen().removePreference(mCtsPref);
+            } else {
+                mCtsPref.setChecked(mCtsEnabled);
             }
         }
 
