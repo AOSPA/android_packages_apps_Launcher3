fa9666d2834a3c526adaaed03ef3a37eba0ea0ee
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-30 18:46:29 +0530

    Launcher3: Skip hotseat width calculation in vertical layout
    In vertical bar layout (phone screen with forced landscape mode),
    hotseat width is calculated incorrectly which incorrectly placed
    icons and not responding to taps. Skip the calculation in this case.
    
    Change-Id: I91f80c9774ebc6906663af00454423425ddd7d0b

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -981,6 +981,8 @@ public class DeviceProfile {
      * necessary.
      */
     public void recalculateHotseatWidthAndBorderSpace() {
+        if (isVerticalBarLayout()) return;
+
         updateHotseatWidthAndBorderSpace(inv.numColumns);
         int numWorkspaceColumns = getPanelCount() * inv.numColumns;
         if (isTwoPanels) {
